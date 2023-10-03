dde8912f68bf385ea4e2c5123c22804c1283b26f
Joachim Kiedrowski <joachim@machenwir.so>
2023-10-03 21:16:22 +0200

    Add tailwind and daisyUI

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    index
+  <div class="container mx-auto p-4">
+    <div class="card w-96 bg-primary text-primary-content mx-auto">
+      <div class="card-body">
+        <h2 class="card-title">Card title!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn">Buy Now</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
