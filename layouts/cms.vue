0a7f842ce67c6258e0dbc71e64e8750a6237fe56
Joachim Kiedrowski <joachim.kie@googlemail.com>
2023-10-16 14:17:21 +0200

    Update cms layout

--- layouts/cms.vue
+++ layouts/cms.vue
@@ -3,19 +3,34 @@
     <div class="grid-container relative container mx-auto min-h-screen">
       <div class="grid-item-nav sticky flex justify-between top-0 h-screen overflow-hidden">
         <div class="flex-1">
-          <div class="h-16">
-            NAV HEADER
+          <div class="h-16 flex justify-center py-2">
+            <CakeIcon class="text-primary" />
           </div>
-          <div class="h-[calc(100%_-_64px)] overflow-scroll pr-4">
-            NAV MENU
+          <div class="h-[calc(100%_-_64px)] overflow-scrolly pr-4">
+            <DMenu :items="items" />
           </div>
         </div>
         <div class="nav-shadow" />
       </div>
-      <div class="grid-item-header sticky top-0 pl-8 bg-white">
-        <div class="h-full">
-          HEADER
-        </div>
+      <div class="grid-item-header sticky top-0 pl-8">
+        <DNavbar>
+          <template #start>
+            <NuxtLink :to="localePath({name: 'index'})" class="btn normal-case text-xl">
+              {{ $t('c.navbar.title') }}
+            </NuxtLink>
+          </template>
+          <template #center />
+          <template #end>
+            <div class="join">
+              <NuxtLink :to="localePath({name: 'login'})" class="join-item btn btn-ghost">
+                {{ $t('c.navbar.login') }}
+              </NuxtLink>
+              <NuxtLink :to="localePath({name: 'registration'})" class="join-item btn btn-ghost">
+                {{ $t('c.navbar.registration') }}
+              </NuxtLink>
+            </div>
+          </template>
+        </DNavbar>
       </div>
       <div class="grid-item-main p-8 pt-0">
         <slot />
@@ -25,13 +40,32 @@
 </template>
 
 <script lang="ts" setup>
+import { AcademicCapIcon, MoonIcon, HomeIcon, CakeIcon } from '@heroicons/vue/24/solid'
+const localePath = useLocalePath()
+
+const items = [
+  {
+    title: 'Home',
+    icon: HomeIcon,
+    to: localePath({ name: 'index' })
+  },
+  {
+    title: 'Home2',
+    icon: AcademicCapIcon,
+    to: localePath({ name: 'inde2' })
+  }, {
+    title: 'Home3',
+    icon: MoonIcon,
+    to: localePath({ name: 'index3' })
+  }
+]
 
 </script>
 
 <style lang="scss">
 
 .nav-shadow {
-  @apply my-48;
+  @apply my-56;
   @apply w-0;
   box-shadow: 100px 0px 120px 120px #ababab52;
 }
@@ -51,7 +85,7 @@
 .grid-container {
   display: grid;
   grid-template-columns: 256px 1fr;
-  grid-template-rows: 60px 1fr;
+  grid-template-rows: 64px 1fr;
   grid-template-areas:
     "nav header"
     "nav content";
