a347870918cdd65d013064ff11196ff608cd9a3c
Joachim Kiedrowski <joachim.kie@googlemail.com>
2023-10-22 13:25:12 +0200

    Add prisma, schema and seeds

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/i18n',
     '@vueuse/nuxt'
   ],
+
   tailwindcss: {
     cssPath: '~/assets/scss/tailwind.scss'
   },
